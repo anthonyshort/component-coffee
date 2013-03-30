5b3abd98ecfd90ee7c67bcb17c9a7eac89335e22
Anthony Short <antshort+github@gmail.com>
2013-03-30 16:17:10 +1100

    Fixed a couple of bugs by making the file read sync

--- index.js
+++ index.js
@@ -11,24 +11,17 @@ module.exports = function(builder) {
     });
 
     // No scripts
-    if( coffee.length === 0 ) return;
+    if( coffee.length === 0 ) return next();
 
     coffee.forEach(function(file, i){
-      fs.readFile(pkg.path(file), 'utf8', function(err, str){
-        if(err) throw new Error(err.toString());
-        var compiled = coffeescript.compile(str, { filename : file, bare: true });
-        var filename = file.replace('.coffee', '.js');
-        pkg.addFile('scripts', filename, compiled);
-        pkg.removeFile('scripts', file);
-        if(i === ( coffee.length - 1 )) next();
-      });
+      var realpath = pkg.path(file);
+      var str = fs.readFileSync(realpath, 'utf8');
+      var compiled = coffeescript.compile(str, { filename : realpath, bare: true });
+      var filename = file.replace('.coffee', '.js');
+      pkg.addFile('scripts', filename, compiled);
+      pkg.removeFile('scripts', file);
     });
 
-    // This is a bit of a hack because Component currently reloads
-    // the conf from the file each time. When this gets fixed we can remove this
-    builder.conf.scripts = builder.conf.scripts.map(function(file){
-      return ( path.extname(file) === '.coffee' ) ? file.replace('.coffee', '.js') : file;
-    });
-
+    next();
   });
 };
