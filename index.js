83e2c11c6fb52b0b8a2b61efdd964749c680b647
Anthony Short <antshort+github@gmail.com>
2013-03-30 14:59:29 +1100

    Fixed issues with nested components

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ var coffeescript = require('coffee-script');
 var path = require('path');
 var fs = require('fs');
 
-function hook(pkg, next) {
-
-  // Get all the coffee files from the scripts list
-  var coffee = pkg.conf.scripts.filter(function(file){
-    return path.extname(file) === '.coffee';
-  });
-
-  // No scripts
-  if( coffee.length === 0 ) return;
-
-  coffee.forEach(function(file, i){
-    fs.readFile(path.resolve(file), 'utf8', function(err, str){
-      if(err) throw new Error(err.toString());
-      var compiled = coffeescript.compile(str, { filename : file, bare: true });
-      var filename = file.replace('.coffee', '.js');
-      pkg.addFile('scripts', filename, compiled);
-      if(i === ( coffee.length - 1 )) next();
-    });
-  });
-
-  // Remove all coffee files and replace with JS extension
-  pkg.conf.scripts = pkg.conf.scripts.map(function(file){
-    return ( path.extname(file) === '.coffee' ) ? file.replace('.coffee', '.js') : file;
-  });
-
-}
-
 module.exports = function(builder) {
-  builder.hook('before scripts', hook);
+  builder.hook('before scripts', function(pkg, next) {
+
+    // Get all the coffee files from the scripts list
+    var coffee = pkg.conf.scripts.filter(function(file){
+      return path.extname(file) === '.coffee';
+    });
+
+    // No scripts
+    if( coffee.length === 0 ) return;
+
+    coffee.forEach(function(file, i){
+      fs.readFile(pkg.path(file), 'utf8', function(err, str){
+        if(err) throw new Error(err.toString());
+        var compiled = coffeescript.compile(str, { filename : file, bare: true });
+        var filename = file.replace('.coffee', '.js');
+        pkg.addFile('scripts', filename, compiled);
+        pkg.removeFile('scripts', file);
+        if(i === ( coffee.length - 1 )) next();
+      });
+    });
+
+    // This is a bit of a hack because Component currently reloads
+    // the conf from the file each time. When this gets fixed we can remove this
+    builder.conf.scripts = builder.conf.scripts.map(function(file){
+      return ( path.extname(file) === '.coffee' ) ? file.replace('.coffee', '.js') : file;
+    });
+
+  });
 };
